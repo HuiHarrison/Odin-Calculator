3ca86bbb7312ace16907e9a7aad965eada586d0d
Harrison <109526528+HuiHarrison@users.noreply.github.com>
2024-02-11 23:40:56 +0800

    Store First Number After an Operator is Clicked

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const numbers = document.querySelectorAll(".numbers");
 const acBtn = document.querySelector("#ac");
 const changeSignBtn = document.querySelector("#changeSignBtn");
 const percentBtn = document.querySelector("#percentBtn");
+const operators = document.querySelectorAll(".operation");
 
+let selectedOperator = null;
+let selectedOperatorFlag = false;
+let num1 = null;
+let num2 = null;
+
+
+function selectNextNum() {
+    operators.forEach(operator => {
+        operator.removeAttribute("id");
+        selectedOperatorFlag = false;
+        num1 = screen.innerText;
+        screen.innerText = "";
+    });
+}
 
 function exceedMaxLength(innerText) {
     return innerText.length > 10
@@ -16,17 +31,34 @@ function addToScreen(num){
 }
 
 // Event listeners for number buttons to add to screen
-for (number of numbers) {
+for (const number of numbers) {
     let numToDisplay = number.innerText;
-    number.addEventListener("click", e => {addToScreen(numToDisplay)});
+    number.addEventListener("click", e => {
+        addToScreen(numToDisplay);
+        if (selectedOperatorFlag) {
+            selectNextNum();
+        }
+    });
 }
 
 // AC Button for clearing screen
-acBtn.addEventListener("click", e => {screen.innerText = ""});
+acBtn.addEventListener("click", e => {
+    screen.innerText = "";
+    selectedOperator = null;
+    selectedOperatorFlag = false;
+    if (selectedOperatorFlag) {
+        selectNextNum();
+    }
+});
 
 
 // +/- Button for changing sign
-changeSignBtn.addEventListener("click", e=> {screen.innerText *= -1;})
+changeSignBtn.addEventListener("click", e=> {
+    screen.innerText *= -1;
+    if (selectedOperatorFlag) {
+        selectNextNum();
+    }
+})
 
 
 // % Button for dividing the number by 100
@@ -34,5 +66,21 @@ percentBtn.addEventListener("click", e => {
     let answer = (screen.innerText / 100).toString();
     if (!exceedMaxLength(answer)) {
         screen.innerText = answer;
+        if (selectedOperatorFlag) {
+            selectNextNum();
+        }
     }
 })
+
+// Highlight operation button if clicked
+for (const operator of operators) {
+    let selectedOperation = operator;
+    operator.addEventListener("click", e => {
+        if (selectedOperatorFlag) {
+            selectNextNum();
+        }
+        selectedOperation.setAttribute("id", "selected-operation");
+        selectedOperatorFlag = true;
+        selectedOperator = selectedOperation.innerText;
+    });
+}
